f2272d32c939c5b947c3b1fce2775bf5a0d34bfc
Sergei <igracom@yandex.ru>
2024-02-18 21:12:15 +0300

    Update Algorithms.py

--- Computing_algorithms/lab_01/Algorithms.py
+++ Computing_algorithms/lab_01/Algorithms.py
@@ -58,51 +58,7 @@ def get_hermit_table(data):
     derivative_count = 2
     table_subs = []
 
-    for point in data:
-        table_subs.append([point.x, point.y])
     
-    y_d_row = []
-    for point in data:
-        y_d_row.append(point.derivative)
-        for j in range(derivative_count):
-            y_d_row.append(None)
-    
-    x_col_id = 0
-    y_col_id = 1
-
-    for i in range(0, len(table_subs) * (derivative_count + 1), (derivative_count + 1)):
-        for j in range(1, (derivative_count + 1)):
-            table_subs.insert(i + j, [])
-    
-    for i in range(0, len(table_subs), (derivative_count + 1)):
-        for j in range(1, (derivative_count + 1)):
-            table_subs[i + j].append(table_subs[i][x_col_id])
-            table_subs[i + j].append(table_subs[i][y_col_id])
-        
-    
-    for i in range(0, len(table_subs) - 2, (derivative_count + 1)):
-        for j in range(1, (derivative_count + 1)):
-            subElement = (table_subs[i][y_col_id] - table_subs[i + j + 1][y_col_id]) / \
-                (table_subs[i][x_col_id] - table_subs[i + j + 1][x_col_id])
-
-            y_d_row[i + j] = subElement
-    
-    table_subs = list([list(row) for row in transpose(table_subs)])
-    x_row = table_subs[0]
-    y_d_row.pop()
-    table_subs.append(y_d_row)
-
-    for args_count in range(2, len(x_row)):
-        table_subs.append([])
-        cur_y_row = table_subs[len(table_subs) - data_cols_count]
-
-        for j in range(0, len(x_row) - args_count):
-            if (abs(x_row[j] - x_row[j + args_count]) < EPS):
-                cur = y_d_row[j]
-            else:
-                cur = (cur_y_row[j] - cur_y_row[j + 1]) / \
-                    (x_row[j] - x_row[j + args_count])
-            table_subs[args_count + data_cols_count - 1].append(cur)
     
     return table_subs
 
