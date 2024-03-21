e550ba27727850cfa2f18b94b219c5b2ef3ff511
Sergei <igracom@yandex.ru>
2024-03-22 00:24:53 +0300

    add for three dots work

---
+++ Computing_algorithms/lab_02/main_2.py
@@ -0,0 +1,22 @@
+from newton_polynom import *
+from spline_algorithm_new import *
+import IO as io
+
+
+data = io.read_table_from_file("./data/data_2.txt")
+io.print_table(data)
+
+
+def func(x):
+    return x**3 - x - 1
+
+
+x = 1.3
+
+start1 = 0
+end1 = 12
+
+print("x ", x)
+
+print("Cплайн 0 и 0:             ", spline(data, x, start1, end1))
+print("Func", func(x))
