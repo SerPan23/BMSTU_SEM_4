d78c5e959e919cdf629ffd786954a33af9188943
Sergei <igracom@yandex.ru>
2024-05-30 22:04:17 +0300

    Update LoadManager.cpp

--- OOP/lab_03/managers/load/LoadManager.cpp
+++ OOP/lab_03/managers/load/LoadManager.cpp
@@ -1,5 +1,6 @@
 #include "LoadManager.h"
 
+#include "LoadException.h"
 #include "ManagerSolution.h"
 #include "SourceSolution.h"
 #include "BuilderSolution.h"
@@ -13,6 +14,9 @@ int LoadManager::loadMeshModel(const std::string &path)
     auto scene = sceneManager->getScene();
 
     auto source = SourceSolution::create(path);
+
+    if (source == nullptr)
+        throw WrongSourceError(__FILE__, __LINE__, "Unknown source type");
 
     auto builder = BuilderSolution::create(path, source);
 
